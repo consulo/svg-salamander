c500e88413398ad51f6df75ab6cb6f6643b6dbdd
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-11-20 17:34:27 +0300

    fixed #2

---
+++ svg-core/src/main/module-info.java
@@ -0,0 +1,13 @@
+module svg.salamander {
+  requires org.slf4j;
+  requires java.desktop;
+
+  exports com.kitfox.svg;
+  exports com.kitfox.svg.app;
+  exports com.kitfox.svg.animation;
+  exports com.kitfox.svg.composite;
+  exports com.kitfox.svg.pathcmd;
+  exports com.kitfox.svg.pattern;
+  exports com.kitfox.svg.util;
+  exports com.kitfox.svg.xml;
+}
